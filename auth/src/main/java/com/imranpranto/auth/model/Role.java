dfd23fe0d58eca4373d5bac375df13f44fae145a
imranpranto00 <kipwalton2000@gmail.com>
2024-10-08 16:52:37 +0600

    auth with spring

---
+++ auth/src/main/java/com/imranpranto/auth/model/Role.java
@@ -0,0 +1,6 @@
+package com.imranpranto.auth.model;
+
+public enum Role {
+USER,
+ADMIN
+}
---
+++ auth/src/main/java/com/imranpranto/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.imranpranto.auth.repository;
+
+
+import com.imranpranto.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
